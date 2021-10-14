a2fca4fde6a99dc35b098fa367a1567592a156db
Nick Stogner <nicholas.stogner@gmail.com>
2021-10-14 10:02:43 -0400

    Add comment

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package timewindow
 
 import "time"
 
+// UntilTomorrow returns the amount of time until midnight.
 func UntilTomorrow(now time.Time) time.Duration {
 	tomorrow := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).Add(24 * time.Hour)
 	return tomorrow.Sub(now)
