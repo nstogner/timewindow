2d78b1ec555da9183d081ef4cad67883f936c5a4
Nick Stogner <nicholas.stogner@gmail.com>
2021-10-14 10:27:13 -0400

    Improve test coverage

--- tod_window_internal_test.go
+++ tod_window_internal_test.go
@@ -14,11 +14,11 @@ func TestTODWindow(t *testing.T) {
 		window TODWindow
 		now    time.Time
 
-		withinWindow  bool
-		duration      time.Duration
-		sameDay       bool
-		startTime     time.Time
-		endTime       time.Time
+		withinWindow       bool
+		duration           time.Duration
+		sameDay            bool
+		startTime          time.Time
+		endTime            time.Time
 		followingStartTime time.Time
 	}{
 		{
@@ -29,11 +29,11 @@ func TestTODWindow(t *testing.T) {
 			},
 			now: time.Date(2000, time.January, 1, 9, 0, 0, 0, time.UTC),
 
-			withinWindow:  false,
-			duration:      time.Hour,
-			sameDay:       true,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
+			withinWindow:       false,
+			duration:           time.Hour,
+			sameDay:            true,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
 			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
 		},
 		{
@@ -44,71 +44,26 @@ func TestTODWindow(t *testing.T) {
 			},
 			now: time.Date(2000, time.January, 1, 9, 0, 0, 0, time.UTC),
 
-			withinWindow:  false,
-			duration:      time.Hour,
-			sameDay:       false,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 2, 8, 0, 0, 0, time.UTC),
+			withinWindow:       false,
+			duration:           time.Hour,
+			sameDay:            false,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 2, 8, 0, 0, 0, time.UTC),
 			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
 		},
 		{
-			name: "one-hour-within-same-day-window",
+			name: "20-minutes-before-window",
 			window: TODWindow{
 				Start: TOD{Hour: 10, Minute: 0},
 				End:   TOD{Hour: 20, Minute: 0},
 			},
-			now: time.Date(2000, time.January, 1, 11, 0, 0, 0, time.UTC),
+			now: time.Date(2000, time.January, 1, 9, 40, 0, 0, time.UTC),
 
-			withinWindow:  true,
-			duration:      23 * time.Hour,
-			sameDay:       true,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
-			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
-		},
-		{
-			name: "one-hour-after-same-day-window",
-			window: TODWindow{
-				Start: TOD{Hour: 10, Minute: 0},
-				End:   TOD{Hour: 20, Minute: 0},
-			},
-			now: time.Date(2000, time.January, 1, 21, 0, 0, 0, time.UTC),
-
-			withinWindow:  false,
-			duration:      13 * time.Hour,
-			sameDay:       true,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
-			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
-		},
-		{
-			name: "one-hour-within-non-same-day-window",
-			window: TODWindow{
-				Start: TOD{Hour: 10, Minute: 0},
-				End:   TOD{Hour: 8, Minute: 0},
-			},
-			now: time.Date(2000, time.January, 1, 11, 0, 0, 0, time.UTC),
-
-			withinWindow:  true,
-			duration:      23 * time.Hour,
-			sameDay:       false,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 2, 8, 0, 0, 0, time.UTC),
-			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
-		},
-		{
-			name: "one-hour-after-non-same-day-window",
-			window: TODWindow{
-				Start: TOD{Hour: 10, Minute: 0},
-				End:   TOD{Hour: 3, Minute: 0},
-			},
-			now: time.Date(2000, time.January, 1, 6, 0, 0, 0, time.UTC),
-
-			withinWindow:  false,
-			duration:      4 * time.Hour,
-			sameDay:       false,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 2, 3, 0, 0, 0, time.UTC),
+			withinWindow:       false,
+			duration:           20 * time.Minute,
+			sameDay:            true,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
 			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
 		},
 		{
@@ -119,11 +74,71 @@ func TestTODWindow(t *testing.T) {
 			},
 			now: time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
 
-			withinWindow:  true,
-			duration:      0,
-			sameDay:       true,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
+			withinWindow:       true,
+			duration:           0,
+			sameDay:            true,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
+			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "one-hour-within-same-day-window",
+			window: TODWindow{
+				Start: TOD{Hour: 10, Minute: 0},
+				End:   TOD{Hour: 20, Minute: 0},
+			},
+			now: time.Date(2000, time.January, 1, 11, 0, 0, 0, time.UTC),
+
+			withinWindow:       true,
+			duration:           23 * time.Hour,
+			sameDay:            true,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
+			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "one-hour-within-non-same-day-window",
+			window: TODWindow{
+				Start: TOD{Hour: 10, Minute: 0},
+				End:   TOD{Hour: 8, Minute: 0},
+			},
+			now: time.Date(2000, time.January, 1, 11, 0, 0, 0, time.UTC),
+
+			withinWindow:       true,
+			duration:           23 * time.Hour,
+			sameDay:            false,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 2, 8, 0, 0, 0, time.UTC),
+			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "one-hour-after-same-day-window",
+			window: TODWindow{
+				Start: TOD{Hour: 10, Minute: 0},
+				End:   TOD{Hour: 20, Minute: 0},
+			},
+			now: time.Date(2000, time.January, 1, 21, 0, 0, 0, time.UTC),
+
+			withinWindow:       false,
+			duration:           13 * time.Hour,
+			sameDay:            true,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
+			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
+		},
+		{
+			name: "one-hour-after-non-same-day-window",
+			window: TODWindow{
+				Start: TOD{Hour: 10, Minute: 0},
+				End:   TOD{Hour: 3, Minute: 0},
+			},
+			now: time.Date(2000, time.January, 1, 6, 0, 0, 0, time.UTC),
+
+			withinWindow:       false,
+			duration:           4 * time.Hour,
+			sameDay:            false,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 2, 3, 0, 0, 0, time.UTC),
 			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
 		},
 		{
@@ -134,26 +149,26 @@ func TestTODWindow(t *testing.T) {
 			},
 			now: time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
 
-			withinWindow:  false,
-			duration:      14 * time.Hour,
-			sameDay:       true,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
+			withinWindow:       false,
+			duration:           14 * time.Hour,
+			sameDay:            true,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
 			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
 		},
 		{
-			name: "20-minutes-before",
+			name: "one-hour-after-end",
 			window: TODWindow{
 				Start: TOD{Hour: 10, Minute: 0},
 				End:   TOD{Hour: 20, Minute: 0},
 			},
-			now: time.Date(2000, time.January, 1, 9, 40, 0, 0, time.UTC),
+			now: time.Date(2000, time.January, 1, 21, 0, 0, 0, time.UTC),
 
-			withinWindow:  false,
-			duration:      20 * time.Minute,
-			sameDay:       true,
-			startTime:     time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
-			endTime:       time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
+			withinWindow:       false,
+			duration:           13 * time.Hour,
+			sameDay:            true,
+			startTime:          time.Date(2000, time.January, 1, 10, 0, 0, 0, time.UTC),
+			endTime:            time.Date(2000, time.January, 1, 20, 0, 0, 0, time.UTC),
 			followingStartTime: time.Date(2000, time.January, 2, 10, 0, 0, 0, time.UTC),
 		},
 	}
